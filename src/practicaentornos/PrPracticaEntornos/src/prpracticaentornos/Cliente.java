3b17a9620722f9965da5b37433c1574a7e1e0296
PabloOller <pop0002@alu.medac.es>
2022-03-31 09:59:47 +0200

    Declaracion de atributos de la clase cliente

---
+++ src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prpracticaentornos;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Cliente {
+        private String nombre;
+    private String apellidos;
+    private int edad;
+    private boolean amable;
+    private boolean atendido;
+}
