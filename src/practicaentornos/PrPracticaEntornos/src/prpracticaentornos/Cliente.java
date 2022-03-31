63992d97a0cc1adf95785ee0aeae47fafe0292b8
PabloOller <pop0002@alu.medac.es>
2022-03-31 10:48:53 +0200

    Correcion de errores y añado 2 funciones en lista clientes

--- src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/Cliente.java
+++ src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/Cliente.java
@@ -2,34 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-<<<<<<< HEAD
-package fruteria;
-=======
 package prpracticaentornos;
->>>>>>> main
 
 /**
  *
  * @author Pablo
  */
 public class Cliente {
-<<<<<<< HEAD
 
     private String nombre;
-=======
-        private String nombre;
->>>>>>> main
     private String apellidos;
     private int edad;
     private boolean amable;
     private boolean atendido;
-<<<<<<< HEAD
 
     public Cliente(String nombre, String apellidos, int edad) {
-=======
-	
-	public Cliente(String nombre, String apellidos, int edad) {
->>>>>>> main
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
@@ -76,7 +63,6 @@ public class Cliente {
     public void setAtendido(boolean atendido) {
         this.atendido = atendido;
     }
-<<<<<<< HEAD
 
     public void atenderCliente() {
         System.out.println("El cliente está comprando...");
@@ -87,7 +73,4 @@ public class Cliente {
     public String toString() {
         return "\nCliente:" + " Nombre:" + this.nombre + " Apellidos: " + this.apellidos + " Edad: " + this.edad;
     }
-
-=======
->>>>>>> main
 }
