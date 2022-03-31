a11fda3200cd98637ff0b99cb574c5a047dedce2
Manuel <mme0008@alu.medac.es>
2022-03-31 10:08:40 +0200

    Constructor de cliente y getter and setter

--- src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/Cliente.java
+++ src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/Cliente.java
@@ -14,4 +14,52 @@ public class Cliente {
     private int edad;
     private boolean amable;
     private boolean atendido;
+	
+	public Cliente(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.amable = true;
+        this.atendido = false;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isAmable() {
+        return amable;
+    }
+
+    public void setAmable(boolean amable) {
+        this.amable = amable;
+    }
+
+    public boolean isAtendido() {
+        return atendido;
+    }
+
+    public void setAtendido(boolean atendido) {
+        this.atendido = atendido;
+    }
 }
