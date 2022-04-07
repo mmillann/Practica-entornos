df4c861418ae66afb1212ba6be6fa1a2ff802bc5
Manuel <mme0008@alu.medac.es>
2022-04-07 09:46:24 +0200

    Instancias

--- src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/PrPracticaEntornos.java
+++ src/practicaentornos/PrPracticaEntornos/src/prpracticaentornos/PrPracticaEntornos.java
@@ -14,7 +14,10 @@ public class PrPracticaEntornos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ListaClientes lista1 = new ListaClientes();
+        Cliente David = new Cliente("David", "Broncano Aguilar", 35);
+        ListaClientes lista2 = new ListaClientes(David);
+        int opcion = 0;
     }
     
 }
