ea7e7a62adf300d017eef9640030d5f7590c3293
Manuel <mme0008@alu.medac.es>
2022-04-28 10:24:10 +0200

    test

---
+++ src/practicaentornos/PrPracticaEntornos/test/Test1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Test1 {
+    
+    public Test1() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
